4835b1f6a77051689399e67a43b497d2fc081470
Brandon Boswell <brandonkboswell@gmail.com>
2013-06-20 16:30:57 -0400

    Added Swap To AppSetup

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -10,4 +10,15 @@ node[:deploy].each do |app_name, deploy|
     EOH
   end
 
+  script "create_swap" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+    sudo /sbin/mkswap /var/swap.1
+    sudo /sbin/swapon /var/swap.1
+    EOH
+  end
+
 end
