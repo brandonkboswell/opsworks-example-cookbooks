6bc7a73c20c7577f0e2ac995eedd10958c050010
Brandon Boswell <brandonkboswell@gmail.com>
2013-06-20 17:57:02 -0400

    Removed Unneccessry Folder

--- mycookbooks/distribute/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'distribute'
-maintainer       'Cont3nt.com'
-maintainer_email 'brandon@cont3nt.com'
-license          'All rights reserved'
-description      'Installs/Configures blah'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
